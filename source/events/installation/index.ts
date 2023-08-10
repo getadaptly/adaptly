af9a09221d59ef3b59b696062825e0dd65843237
Lauris Skraucis <lauris@Lauriss-MacBook-Pro.local>
2023-08-10 12:31:08 +0200

    refactor: event handlers responsible for extracting request data

--- source/events/installation/index.ts
+++ source/events/installation/index.ts
@@ -1,7 +1,13 @@
+import { throwRequestMissingInformation } from '@adaptly/errors/shared';
 import { created } from '@adaptly/events/installation/actions/created';
-import { InstallationCreatedEvent } from '@octokit/webhooks-types';
+import { InstallationEvent } from '@octokit/webhooks-types';
+import { Request } from 'express';
 
-export const installationHandler = async (payload: InstallationCreatedEvent) => {
+export const INSTALLATION_EVENT = 'installation';
+
+export const installationHandler = async (req: Request) => {
+    const payload = getPayload(req);
+
     switch (payload.action) {
         case 'created':
             await created(payload);
@@ -10,3 +16,23 @@ export const installationHandler = async (payload: InstallationCreatedEvent) => 
             break;
     }
 };
+
+function getPayload(req: Request): InstallationEvent {
+    const payload = req.body;
+
+    try {
+        if (!payload) {
+            throw new Error('Payload is missing');
+        }
+
+        const eventType = req.headers['x-github-event'] as string;
+
+        if (eventType !== INSTALLATION_EVENT) {
+            throw new Error('Event type invalid');
+        }
+    } catch (error) {
+        throwRequestMissingInformation(error);
+    }
+
+    return payload;
+}
