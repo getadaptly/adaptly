874f52cc6f19b377efc620c9b880a496fbbe6c11
Mastro Geppetto <141870054+gerpetto@users.noreply.github.com>
2023-08-21 10:15:37 +0100

    feat: add helicone for monitoring of OpenAI requests (#86)

--- source/services/openai/client.ts
+++ source/services/openai/client.ts
@@ -8,6 +8,15 @@ export const MAX_NUM_TOKENS_16K = 16383;
 export const GPT35_MODEL = 'gpt-3.5-turbo-16k-0613';
 export const GPT4_MODEL = 'gpt-4-0613';
 
-const configuration = new Configuration({ apiKey: getEnv('OPENAI_API_KEY') });
+const configuration = new Configuration({
+    apiKey: getEnv('OPENAI_API_KEY'),
+    basePath: 'https://oai.hconeai.com/v1',
+    baseOptions: {
+        headers: {
+            'Helicone-Auth': `Bearer ${getEnv('HELICONE_API_KEY')}`,
+            'Helicone-Property-App': getEnv('ENVIRONMENT')
+        }
+    }
+});
 
 export const openai = new OpenAIApi(configuration);
